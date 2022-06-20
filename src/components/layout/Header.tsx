a7b5127a8d4f4216d9d52b58bd0ffffe892fe24e
Mert Yılmaz <myyilmaz97@gmail.com>
2022-06-21 00:01:24 +0300

    Added .env to gitignore and updated header fonts

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between max-w-lg p-10 ">
       <img src={AppLogo} alt="logo" className="w-14 h-14" />
-      <div className="flex gap-6 text-lg font-medium text-center">
+      <div className="flex gap-6 text-xl font-medium text-center">
         <Link
           to="/"
           className="hover:underline underline-offset-4 decoration-primary decoration-4 "
